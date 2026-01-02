9dcc4c6f66bea05d1e158b585a6458646f580551
Dca <pikirsendiri953@gmail.com>
2026-01-02 20:32:59 +0700

    product 1

--- products.js
+++ products.js
@@ -1,20 +1,38 @@
 window.PRODUCTS = [
   {
-    title: "Produk A",
-    price: "Rp 25.000",
-    img: "../shopee.png",
-    link: "https://shopee.co.id/produk-a"
+    title: "Jas Hujan Setelan Baju Celana Pria Wanita Tebal Kuat Anti Rembes by AKSARA Original",
+    price: "Rp 75.000",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-81ztg-meqqofinmkg04a.webp",
+    link: "https://s.shopee.co.id/6KxsRWIQ6O"
   },
   {
-    title: "Produk B",
-    price: "Rp 40.000",
-    img: "shopee.png",
-    link: "https://shopee.co.id/produk-b"
+    title: "KAHF NATURAL DISCOVERY FROM EVERYDAY LIFE FACE WASH",
+    price: "Rp 26.675 - Rp 36.300",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-81ztf-mf0uv4o7bjt4a9.webp",
+    link: "https://s.shopee.co.id/6KxsSbqOxd"
   },
   {
-    title: "Produk C",
-    price: "Rp 15.000",
-    img: "shopee.png",
-    link: "https://shopee.co.id/produk-c"
+    title: "DANYCASE Kipas Angin Portable Mini Tangan Kecil Turbo",
+    price: "Rp 55.513 - Rp 79.572",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-7rbk8-manmvr8bx3h54b.webp",
+    link: "https://s.shopee.co.id/qcvuWOcGL"
+  },
+  {
+    title: "TrailTop Kursi Lipat Outdoor Serbaguna Kursi Lipat Camping Portable",
+    price: "Rp 47.493",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-7r98q-lzsui3k0vmcoc6.webp",
+    link: "https://s.shopee.co.id/8pfDRFB0no"
+  },
+  {
+    title: "PROMO USUS CRISPY",
+    price: "Rp 56.000 - Rp 97.000",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-81zth-meds94i963gja2.webp",
+    link: "https://s.shopee.co.id/3fx7HleWJR"
+  },
+  {
+    title: "LITTLE O Tumbler STARBUCKS Tumbler Stainley",
+    price: "Rp 71.250 - Rp 185.150",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-81ztd-mepdnomeywow25.webp",
+    link: "https://s.shopee.co.id/8APWe3UCfe"
   }
 ];
