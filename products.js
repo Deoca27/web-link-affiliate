cc1c9efda630a315ff46bea297702ad34136d922
Dca <pikirsendiri953@gmail.com>
2026-01-03 15:45:43 +0700

    produk2

--- products.js
+++ products.js
@@ -34,5 +34,41 @@ window.PRODUCTS = [
     price: "Rp 71.250 - Rp 185.150",
     img: "https://down-id.img.susercontent.com/file/id-11134207-81ztd-mepdnomeywow25.webp",
     link: "https://s.shopee.co.id/8APWe3UCfe"
+  },
+  {
+    title: "ASADTSU Jas Hujan Ransel RIB Setelan Dewasa Pria Wanita",
+    price: "Rp 181.255",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-7r98r-lzvv3jp4g74824.webp",
+    link: "https://s.shopee.co.id/5L5MUIRFkU"
+  },
+  {
+    title: "Payung Lipat Anti Sinar UV & Tahan Hujan",
+    price: "Rp 51.900",
+    img: "https://down-id.img.susercontent.com/file/sg-11134201-824j0-mf0zuemvfjm150.webp",
+    link: "https://s.shopee.co.id/2B8KiWcAIz"
+  },
+  {
+    title: "DANYCASE Kipas Angin Meja Aromaterapi",
+    price: "Rp 52.445 - Rp 198.998",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-7rbke-maxlpe2zdickbe.webp",
+    link: "https://s.shopee.co.id/4q95tSCQQE"
+  },
+  {
+    title: "Jersey Bola Timnas Indonesia",
+    price: "Rp 117.520 - Rp 149.500",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-7rbk5-ma3yuwydvod904.webp",
+    link: "https://s.shopee.co.id/AKU2RiAGLV"
+  },
+  {
+    title: "Cuka Apel Gemeli Premium 500ml With Mother",
+    price: "Rp 19.795 - Rp 25.984",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-7r98x-lrzh9f0s5wzqa9.webp",
+    link: "https://s.shopee.co.id/1LZDjEG1ty"
+  },
+  {
+    title: "Sepatu Lari ATR 7 Pria Wanita",
+    price: "Rp 165.000",
+    img: "https://down-id.img.susercontent.com/file/id-11134207-7rbk6-macmhdxw0ybc50.webp",
+    link: "https://s.shopee.co.id/qcx8KW2CN"
   }
 ];
